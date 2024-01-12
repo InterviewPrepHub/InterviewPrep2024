df13c6bfcdc548260f70fa59f1c5b866be8eaf3d
Divya Paliwal <div.pali@gmail.com>
2024-01-12 19:49:00 +0530

    Interview 2024

---
+++ src/main/java/com/series/InterviewPrep2024/InterviewPrep2024Application.java
@@ -0,0 +1,13 @@
+package com.series.InterviewPrep2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPrep2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPrep2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/series/InterviewPrep2024/InterviewPrep2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.series.InterviewPrep2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPrep2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
